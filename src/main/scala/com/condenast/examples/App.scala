54ab963ed495e2f31b8cffcf1b7b9f247f5a089f
Paras Mehra <paras.mehra@datastax.com>
2018-11-14 15:21:41 -0500

    case class added w/ protobuff

--- src/main/scala/com/condenast/examples/App.scala
+++ src/main/scala/com/condenast/examples/App.scala
@@ -1,6 +1,10 @@
 package com.condenast.examples
 
 import java.nio.ByteBuffer
+
+import com.datastax.driver.core.SimpleStatement
+import com.datastax.driver.dse.DseSession
+import com.datastax.spark.connector.cql.CassandraConnector
 import com.google.protobuf.Any
 import org.apache.spark.sql.{Dataset, SparkSession}
 
@@ -24,6 +28,20 @@ object App {
     val datasetBytes: Dataset[serVal] = featuresRaw.select("serialized_value").as[serVal]
     val features = datasetBytes.map(x => Any.parseFrom(ByteBuffer.wrap(x.serialized_value)))
 
+    val myDeletes = featuresRaw.select("marked_for_delete == True")
+
+    case class test(tinker: String)
+
+    val connector = CassandraConnector(featuresRaw.sparkSession.sparkContext.getConf)
+    myDeletes.foreachPartition(partitionOfUsers => {
+      connector.withSessionDo( {cassandraSession =>
+        val dseSession = cassandraSession.asInstanceOf[DseSession]
+        partitionOfUsers.foreach(delete => {
+          val pk = test(delete.getString(0))
+          val stmt = new SimpleStatement(s"DELETE where ${pk}")
+        })
+      })
+    })
 
   }
 }
